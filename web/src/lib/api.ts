db80c73472c80a2af25fcc73b9ddd449b5fdbb57
souieh <souieh.aissa@gmail.com>
2026-01-10 01:04:26 +0100

    first commit

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -2,7 +2,7 @@ import { authService } from "@/services/auth";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || "/api",
   headers: { "Content-Type": "application/json" },
   timeout: 7000,
 });
